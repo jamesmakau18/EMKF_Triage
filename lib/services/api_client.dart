0ac7be1fcdc5dcebd49a695bacb9a8ecfb0b086c
James Makau <james.makau@pilgrimconsulting.team>
2026-07-09 21:16:13 +0300

    refactor: simplify MockApiClient by removing ChangeNotifier and switching to immutable configuration

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -1,24 +1,18 @@
 import 'dart:math';
-import 'package:flutter/foundation.dart';
 import '../models/triage_record.dart';
 
-class MockApiClient extends ChangeNotifier {
+class MockApiClient {
   final Random _random = Random();
-  bool _simulateFailures = false;
+  final bool simulateFailures;
 
-  bool get simulateFailures => _simulateFailures;
-
-  void toggleFailures(bool value) {
-    _simulateFailures = value;
-    notifyListeners();
-  }
+  MockApiClient({this.simulateFailures = false});
 
   Future<bool> submitTriage(TriageRecord record) async {
     // Artificial 2-second delay
     await Future.delayed(const Duration(seconds: 2));
 
     // Random failure simulation (50% chance to fail)
-    if (_simulateFailures && _random.nextDouble() < 0.5) {
+    if (simulateFailures && _random.nextDouble() < 0.5) {
       throw Exception('Simulated network failure on POST /api/v1/triage');
     }
 
